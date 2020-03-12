53c7023031afa7e364c64f0b09f4586b96efe606
Corey Knoettgen <cknoettg@yahoo.com>
2020-03-11 21:16:37 -0400

    added initial variables to digital timer.

---
+++ stretch-assignment/digital_timer/index.js
@@ -0,0 +1,6 @@
+let secTens = document.selectElementbyId('#secondTens');
+let secOnes = document.selectElementbyId('#secondOnes');
+let colons = document.selectElementbyId('#colon');
+let msHuns = document.selectElementbyId('#msHundreds');
+let msTens = document.selectElementbyId('#msTens');
+
