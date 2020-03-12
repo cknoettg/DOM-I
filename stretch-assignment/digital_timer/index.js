c2a11c3e760142baae6550c251aa0f4ada880cf5
Corey Knoettgen <cknoettg@yahoo.com>
2020-03-11 22:42:00 -0400

    Working on getting the count to loop right.

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,18 +1,41 @@
-let secTens = document.selectElementbyId('#secondTens');
-let secOnes = document.selectElementbyId('#secondOnes');
-let colons = document.selectElementbyId('#colon');
-let msHuns = document.selectElementbyId('#msHundreds');
-let msTens = document.selectElementbyId('#msTens');
+let secTens = document.getElementById('secondTens');
+let secOnes = document.getElementById('secondOnes');
+let colons = document.getElementById('colon');
+let msHuns = document.getElementById('msHundreds');
+let msTens = document.getElementById('msTens');
 
 //example - perform function every 3 secs, 1000ms = 1 sec
 //setInterval(function(){ alert("Hello"); }, 3000);
 
 //counts up to 10 seconds
+const maxTime = 10;
+const maxCount = 9;
+
+//initial test
+//msTens.style.color = "green";
+
+//count up the msTens
+msTens.textContent=0;
+setInterval(function(){
+    for (let i=0;i<=maxCount;i++){
+        msTens.textContent = i.toString();
+        // if (i===maxCount){
+        //     i=0;
+        // }
+        //infinite loop
+    } 
+    //reset count here?
+    //i=0;
+}, 10);
+
+//count up the msHundreds
+
 
 //do something every 10ms
 //setInterval(function(){  }, 10);
+
 // increments every 10ms
-setInterval(function(){  }, 10);
+//setInterval(function(){  }, 10);
 
 //Has digits change to red when it gets to 10 seconds
 
