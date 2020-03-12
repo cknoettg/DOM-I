de62669e6efb704e2b681187291fd5b9bb9454ef
Corey Knoettgen <cknoettg@yahoo.com>
2020-03-11 21:29:32 -0400

    drew up a project plan

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -4,3 +4,16 @@ let colons = document.selectElementbyId('#colon');
 let msHuns = document.selectElementbyId('#msHundreds');
 let msTens = document.selectElementbyId('#msTens');
 
+//example - perform function every 3 secs, 1000ms = 1 sec
+//setInterval(function(){ alert("Hello"); }, 3000);
+
+//counts up to 10 seconds
+
+//do something every 10ms
+//setInterval(function(){  }, 10);
+// increments every 10ms
+setInterval(function(){  }, 10);
+
+//Has digits change to red when it gets to 10 seconds
+
+//Should not count past 10 seconds
